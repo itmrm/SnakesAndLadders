9d5f164cce6ab904417a7ddff621aa57748e51f2
Itamar <itmrm7@gmail.com>
2016-08-04 01:40:00 +0300

    Changed links

--- app/src/main/java/com/eyalin/snakes/WelcomeActivity.java
+++ app/src/main/java/com/eyalin/snakes/WelcomeActivity.java
@@ -11,10 +11,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.eyalin.snakes.BL.Game;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     static final String tag = "WelcomeActivity";
     static final String BUNDLE_KEY = "welcome";
+    static final String MODE_KEY = "mode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +27,17 @@ public class WelcomeActivity extends AppCompatActivity {
 
     public void onSinglePlayerClicked(View view) {
         Intent intent = new Intent(WelcomeActivity.this, GameActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt(MODE_KEY, 0);
+        intent.putExtra(BUNDLE_KEY, bundle);
         startActivity(intent);
     }
 
     public void onMultiPlayerClicked(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, GameActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt(MODE_KEY, 0);
+        intent.putExtra(BUNDLE_KEY, bundle);
         startActivity(intent);
     }
 
